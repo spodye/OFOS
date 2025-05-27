89d73a5f8802865303d62145ccec6569b0982472
Spodye <Pokharelspandan@gmail.com>
2025-05-27 18:38:19 +0545

    removed comment

--- app/Livewire/Menu.php
+++ app/Livewire/Menu.php
@@ -6,10 +6,7 @@ use Livewire\Component;
 use App\Models\Product;
 class Menu extends Component
 {
-    // public function addToOrder($id){
-    //     $dish=Product::where()
-
-    // }
+    
     public function render()
     {
         return view('livewire.menu',['dishes'=>Product::all()]);
